7019e53092b530b761eeefbf4f97f72fda845048
Jonas Liljegren <jonas@liljegren.org>
2013-04-02 19:21:23 +0200

    Initial revision

---
+++ header.php
@@ -0,0 +1,149 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * Header Template
+ *
+ *
+ * @file           header.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2013 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.3
+ * @filesource     wp-content/themes/responsive/header.php
+ * @link           http://codex.wordpress.org/Theme_Development#Document_Head_.28header.php.29
+ * @since          available since Release 1.0
+ */
+?>
+<!doctype html>
+<!--[if !IE]>      <html class="no-js non-ie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9 ]>    <html class="no-js ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+
+<title><?php wp_title('&#124;', true, 'right'); ?></title>
+
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<?php wp_enqueue_style('responsive-style', get_stylesheet_uri(), false, '1.9.0');?>
+
+<?php wp_head(); ?>
+<?php 
+/* AD2013
+** Lade till ad-style.css och länkar till BBPress och BuddyPress CSS där. För att kunna ändra på ett smidigt sätt.
+** CSS-en ligger i temamappen och länkar till ad-bbpress.css och ad-buddypress.css*/ ?>
+<link rel="stylesheet" href="<?php echo home_url('/wp-content/themes/responsive/ad-style.css'); ?>" type="text/css" media="screen" />
+</head>
+
+<body <?php body_class(); ?>>
+                 
+<?php responsive_container(); // before container hook ?>
+<div id="container" class="hfeed">
+         
+    <?php responsive_header(); // before header hook ?>
+    <div id="header">
+    
+     <div class="top-icon-wrapper-responsive">
+    	<a href="#"><div class="dagens-rost-responsive">Dagens röst</div></a>    	
+        <a href="/manifesto/"><div class="ad-in-english-responsive">In english</div></a>
+        <?php // AD2013 Visa vid 980 resolution
+			$user_ID = get_current_user_id();
+			if ($user_ID == 0) {
+				$login_url = '/medlem/anvandare/'. $user_ID . '/profile/edit/';
+       			echo '<a href="'. wp_login_url () .'"><div class="ad-login-link-responsive">Logga in</div></a>';
+			} else {
+				$logout_url = '/du-har-loggat-ut/';
+				echo '<a href="'. wp_logout_url() .'"><div class="ad-logout-link-responsive">Logga ut</div></a>';
+			} 
+		?>        
+    </div>  
+
+		<?php responsive_header_top(); // before header content hook ?>
+    
+        <?php if (has_nav_menu('top-menu', 'responsive')) { ?>
+	        <?php wp_nav_menu(array(
+				    'container'       => '',
+					'fallback_cb'	  =>  false,
+					'menu_class'      => 'top-menu',
+					'theme_location'  => 'top-menu')
+					); 
+				?>
+        <?php } ?>
+        
+    <?php responsive_in_header(); // header hook ?>
+   
+	<?php if ( get_header_image() != '' ) : ?>
+               
+        <div id="logo">
+            <a href="<?php echo home_url('/'); ?>"><img src="<?php header_image(); ?>" width="<?php if(function_exists('get_custom_header')) { echo get_custom_header() -> width;} else { echo HEADER_IMAGE_WIDTH;} ?>" height="<?php if(function_exists('get_custom_header')) { echo get_custom_header() -> height;} else { echo HEADER_IMAGE_HEIGHT;} ?>" alt="<?php bloginfo('name'); ?>" /></a>
+        </div><!-- end of #logo -->
+        
+    <?php endif; // header image was removed ?>
+
+    <?php if ( !get_header_image() ) : ?>
+                
+        <div id="logo">
+            <span class="site-name"><a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></span>
+            <span class="site-description"><?php bloginfo('description'); ?></span>
+        </div><!-- end of #logo -->  
+
+    <?php endif; // header image was removed (again) ?>
+    
+    <?php $logout_url = '/du-har-loggat-ut/'; ?>
+    
+    <div class="ad-login-in-english hide-980">
+        <a href="/manifesto/"><div class="ad-in-english"></div></a>
+        <?php 
+			$user_ID = get_current_user_id();
+			if ($user_ID == 0) {
+       			echo '<a href="'. wp_login_url () .'"><div class="ad-login-link"></div></a>';
+			} else {
+				$logout_url = '/du-har-loggat-ut/';
+				echo '<a href="'. wp_logout_url() .'"><div class="ad-logout-link"></div></a>';
+			} ?>
+    </div>        
+        <a href="#"><div class="dagens-rost hide-980"></div></a>
+        
+     
+        
+    
+    <?php get_sidebar('top'); ?>
+			    
+				<?php wp_nav_menu(array(
+				    'container'       => '',
+					'theme_location'  => 'header-menu')
+					); 
+				?>
+            <div id="search-in-menu" class="widget_search">	<form method="get" id="searchform" action="http://new.aktivdemokrati.se/">
+                <input type="text" class="field menu-search-field" name="s" id="s" placeholder="Sök här &hellip;" />
+                <input type="submit" class="submit menu-submit" name="submit" id="searchsubmit" value="Sök"  />
+            </form>
+            </div>
+                
+            <?php if (has_nav_menu('sub-header-menu', 'responsive')) { ?>
+	            <?php wp_nav_menu(array(
+				    'container'       => '',
+					'menu_class'      => 'sub-header-menu',
+					'theme_location'  => 'sub-header-menu')
+					); 
+				?>
+            <?php } ?>
+
+			<?php responsive_header_bottom(); // after header content hook ?>
+ 
+    </div><!-- end of #header -->
+    <?php responsive_header_end(); // after header container hook ?>
+    
+	<?php responsive_wrapper(); // before wrapper container hook ?>
+    <div id="wrapper" class="clearfix">
+		<?php responsive_wrapper_top(); // before wrapper content hook ?>
+		<?php responsive_in_wrapper(); // wrapper hook ?>
---
+++ sidebar-home.php
@@ -0,0 +1,39 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * Home Widgets Template
+ *
+ *
+ * @file           sidebar-home.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2013 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/sidebar-home.php
+ * @link           http://codex.wordpress.org/Theme_Development#Widgets_.28sidebar.php.29
+ * @since          available since Release 1.0
+ */
+?>  
+	<?php responsive_widgets_before(); // above widgets container hook ?>
+    <div id="widgets" class="home-widgets">
+        <div class="grid col-940">
+        <?php responsive_widgets(); // above widgets hook ?>
+            
+            <?php if (!dynamic_sidebar('home-widget-1')) : ?>
+            <div class="widget-wrapper">
+            
+                <div class="widget-title-home"><h3><?php _e('Home Widget 1', 'responsive'); ?></h3></div>
+                <div class="textwidget"><?php _e('This is your first home widget box. To edit please go to Appearance > Widgets and choose 6th widget from the top in area 6 called Home Widget 1. Title is also manageable from widgets as well.','responsive'); ?></div>
+            
+			</div><!-- end of .widget-wrapper -->
+			<?php endif; //end of home-widget-1 ?>
+
+        <?php responsive_widgets_end(); // responsive after widgets hook ?>
+        </div><!-- end of .col-940 -->
+        
+    </div><!-- end of #widgets -->
+	<?php responsive_widgets_after(); // after widgets container hook ?>
